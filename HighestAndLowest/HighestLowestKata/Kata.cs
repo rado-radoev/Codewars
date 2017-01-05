bfce27babac0bc304ab455f3f57d5d1fc9df8bdc
Radoslav Radoev <rradoev@gmail.com>
2017-01-05 11:16:26 -0800

    Final solution

---
+++ HighestAndLowest/HighestLowestKata/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestLowestKata
+{
+    class Kata
+    {
+        public static string HighAndLow(string numbers)
+        {
+            string[] nums = numbers.Split(' ');
+            int[] numbs = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numbs[i] = Int32.Parse(nums[i]);
+            }
+
+            Array.Sort(numbs);
+
+
+            return (numbs[numbs.Length - 1] + " " + numbs[0]);
+        }
+    }
+}
---
+++ HighestAndLowest/HighestLowestKata/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestLowestKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.HighAndLow("1 2 -3 4 5"));
+        }
+    }
+}
