9fc1c894e6d0e797e0575ef1b3a8f20fc00390b1
Radoslav Radoev <rradoev@gmail.com>
2017-03-08 12:09:59 -0800

    Playing with passphrases

---
+++ Java/Playing with passphrases/src/PlayPass.java
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/playing-with-passphrases/train/java
+
+/**
+  	shift each letter by a given number but the transformed letter must be a letter (circular shift),
+	replace each digit by its complement to 9,
+	keep such as non alphabetic and non digit characters,
+	downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
+	reverse the whole result.
+ 	@author regradra
+
+ */
+public class PlayPass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
