2fd7538e99f0f67a833e9500819c7e5d21667e51
Radoslav Radoev <rradoev@gmail.com>
2017-03-09 08:33:34 -0800

    Rainfall kata start

---
+++ Java/Rainfall/src/Rainfall.java
@@ -0,0 +1,21 @@
+/**
+ * @author regradra
+ * Kata link: https://www.codewars.com/kata/rainfall/train/java
+ */
+public class Rainfall {
+
+    public static double mean(String town, String strng) {
+        // your code
+    }
+    public static double variance(String town, String strng) {
+        // your code
+    }
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
