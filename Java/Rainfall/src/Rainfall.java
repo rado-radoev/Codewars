fcd8c98b37c51ea9d3514688e69ab5b7a69a5737
superklamer <rradoev@gmail.com>
2017-03-09 22:45:38 -0800

    rainfall array and regular expressions ready

--- Java/Rainfall/src/Rainfall.java
+++ Java/Rainfall/src/Rainfall.java
@@ -1,6 +1,10 @@
 // https://www.codewars.com/kata/rainfall/train/java
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+
 public class Rainfall {
-	
+		
 	 public static String data = 
 		     "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9" +
 		     "\n" +
@@ -51,6 +55,7 @@ public class Rainfall {
 //    }
 
 	public static void main(String[] args) {
+
 		// 1. Must breakdown data and data1 strings to:
 		// 	a) Records (separated by new line)
 		//  b) Towns (end with : )
@@ -62,6 +67,7 @@ public class Rainfall {
 		// Regex for the month: [a-zA-Z]+[^\ ]
 		// Regex for the temp:  [0-9]+.[^,]   \d+\.\d+
 		// Regex for new line: 	(^.+)
+		//http://www.ocpsoft.org/opensource/guide-to-regular-expressions-in-java-part-1/
 		
 		
 		// Create hash map that has a month as a key and number as value, hash map will be named as the city name
@@ -71,7 +77,53 @@ public class Rainfall {
 		// when you find the city loop thorugh that hashmap
 		// do the magic on every value for every key
 		
-		System.out.println(data);
+		String[] s = data.split("\n");
+		
+		for (String str : s) {
+			Pattern city = Pattern.compile("^[a-zA-Z]+[^:]");
+			Matcher matchCity = city.matcher(str);
+			
+			// Getting the city name and adding it to a List
+			List<String> cities = new ArrayList<>();
+			while (matchCity.find()) {
+				 System.out.println("Found city: " + matchCity.group());
+				 //cities.add(matchCity.group()); 
+			}
+			
+			
+			// Getting the month
+			Pattern month = Pattern.compile("(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)");
+			Matcher matchMonth = month.matcher(str);
+			while (matchMonth.find()) {
+				System.out.println(matchMonth.group());
+			}
+			
+			
+			// Getting the rain data
+			Pattern rainData = Pattern.compile("[0-9]+.[^,]");
+			Matcher matchRainData = rainData.matcher(str);
+			while (matchRainData.find()) {
+				System.out.println(matchRainData.group());
+			}
+			
+		}
+		
+//
+//		
+//		// Getting the month
+//		Pattern month = Pattern.compile("(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", Pattern.CASE_INSENSITIVE);
+//		Matcher matchMonth = month.matcher(data);
+//		while (matchMonth.find()) {
+//			//System.out.println(matchMonth.group());
+//		}
+//		
+//		
+//		// Getting the rai data
+//		Pattern rainData = Pattern.compile("[0-9]+.[^,]", Pattern.CASE_INSENSITIVE);
+//		Matcher matchRainData = rainData.matcher(data);
+//		while (matchRainData.find()) {
+//			System.out.println(matchRainData.group());
+//		}
 	}
 
 }
