def3e575c4013e1ee3b2cb61f3a12324cb7561a6
Radoslav Radoev <rradoev@gmail.com>
2017-03-13 14:47:46 -0700

    exercism acronym test - done

--- Java/Acronyms/src/Acronym.java
+++ Java/Acronyms/src/Acronym.java
@@ -3,41 +3,25 @@
 public class Acronym {
 	
 	public static String generate(String text) {
-		StringBuilder acronym = new StringBuilder();
-		
-		String[] words = text.split("\\W");
+		StringBuilder acronym = new StringBuilder(); // Empty string to hold the acronym
+		String[] words = text.split("\\W+"); // split the string at all non letter chars
 
-		for (String s : words) {
-			if (!s.isEmpty())
-			{
-				
-				if (s.matches("^[A-Z \\d\\W]+$")) {
-					acronym.append(s.substring(0, 1));
-					continue;
-				}
-				
-				int charIndex = 0;
-				
-				for (int i = 0; i < s.length();i++) {
-					char currentChar = s.charAt(i);
-					if (Character.isUpperCase(currentChar)) {
-						charIndex = i;
-						
-						if (charIndex >= 0) {
-							acronym.append(s.substring(charIndex, charIndex + 1));
-						}
+		for (String s : words) { // for each splitted string
+			if (s.matches("^[A-Z \\d\\W]+$")) { // If the string is all upper case
+				acronym.append(s.substring(0, 1));	// Add the 1st letter to acronym 
+			}
+			else if (s.matches("^[a-z \\d\\W]+$")) { // if the next word is all lower case
+				acronym.append(s.substring(0, 1)); // add the 1st letter to acronym
+			}
+			else { // If the word is not all uppercase and not all lowercase
+				for (int letter = 0; letter < s.length();letter++) { // Check each letter
+					char currentChar = s.charAt(letter); // Assign letter at index to variable
+					if (Character.isUpperCase(currentChar)) { // If letter is uppercase
+						acronym.append(currentChar); // Add it to the acronym
 					}
 				}
-				
-
-				
-			} // End IF
+			}
 		}
 		return acronym.toString().toUpperCase();
-	}
-	
-	public static void main (String[] args) {
-		System.out.println(Acronym.generate("Cat in a Hat"));
-		System.out.println(Acronym.generate("Ruby on Rails"));
 	}
 }
