9b4877eb1740427264a6fbd726cd955d201127d4
Radoslav Radoev <rradoev@gmail.com>
2017-03-14 11:09:28 -0700

    Bit map. May be I can use bitSet class or just bitwise operations

--- Java/secret-handshake/src/HandshakeCalculator.java
+++ Java/secret-handshake/src/HandshakeCalculator.java
@@ -1,9 +1,16 @@
-
+import java.util.BitSet;
 public class HandshakeCalculator {
 
 	public static void main(String[] args) {
-		ArrayList<>
-
+		int number = 16;
+		//System.out.println(Integer.toBinaryString(15));
+		
+		BitSet bit = new BitSet();
+		bit.set(number);
+		System.out.println(bit.size());
+		
+		// E tova chukane chakah
+		//https://www.cs.uky.edu/~keen/intro/binary-to-decimal.html
 	}
 
 }
