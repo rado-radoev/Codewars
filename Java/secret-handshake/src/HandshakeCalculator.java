3ae307ccc722b75dc18bd6a3b7cc8e1a0fb329a7
Radoslav Radoev <rradoev@gmail.com>
2017-03-14 11:20:03 -0700

    Action plan

--- Java/secret-handshake/src/HandshakeCalculator.java
+++ Java/secret-handshake/src/HandshakeCalculator.java
@@ -11,6 +11,27 @@ public class HandshakeCalculator {
 		
 		// E tova chukane chakah
 		//https://www.cs.uky.edu/~keen/intro/binary-to-decimal.html
+		
+		// Or try this:
+		//http://stackoverflow.com/questions/1092411/java-checking-if-a-bit-is-0-or-1-in-a-long
+		
+		//Plan of action
+		/* When you receive the number 
+		 * Check if the 1st bit is set to 1
+		 * if it is add Wink to array
+		 * check if 2nd bit is set to 1
+		 * if it is add double wink to array
+		 * check if 3rd bit is set to 1
+		 * if it is add close your eyes to array
+		 * check if 4th bit is set to 1
+		 * if it is add jump to array
+		 * check if 6th bit is set to 1
+		 * if it is reverse the array
+		 * no more checking
+		 * */
+		
+	
+	
 	}
 
 }
