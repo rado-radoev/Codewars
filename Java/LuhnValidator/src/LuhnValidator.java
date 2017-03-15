ec322c613e864d34e2d47bb4d7db21eb466942dd
superklamer <rradoev@gmail.com>
2017-03-14 21:14:43 -0700

    luhnvalidator - plan of action - TO DO: code it

--- Java/LuhnValidator/src/LuhnValidator.java
+++ Java/LuhnValidator/src/LuhnValidator.java
@@ -2,7 +2,16 @@
 public class LuhnValidator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// regex to check string has only spaces \\d+\\s - if contains other chars and stuff. not valid return false
+		// strip spaces
+		// convert string to int
+		// add to array digit by digit % 10 and / 10
+		// every second(even) digit is multiplied can do bitwise operation <<1
+		// if the result is greater than 9 subtract 9 from result and 
+		// rewrite this digit
+		// sum all the digits
+		// if sum % 10 == 0
+		// number is valid - return true;
 
 	}
 
