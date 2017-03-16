3030d97bd83beab15c8e058214894a8ebe328a5b
Radoslav Radoev <rradoev@gmail.com>
2017-03-16 16:13:58 -0700

    Matrix -have to figure out what methods and what return types we need

--- Java/Matrix/src/Matrix.java
+++ Java/Matrix/src/Matrix.java
@@ -3,36 +3,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Matrix {
+	
+	private static ArrayList<String> rows = new ArrayList<String>();
+	private static ArrayList<String> columns = new ArrayList<String>();
+	
+	public Matrix(String matrixAsString) {
+		calculateColumn(matrixAsString);
+		calculateRow(matrixAsString);
+	}
 
-	public static void main(String[] args) {
-		ArrayList<String> rows = new ArrayList<String>();
-		ArrayList<String> columns = new ArrayList<String>();
-		
-		String matrix = "9 8 7\n5 3 2\n6 6 7";
 
-		Pattern p = Pattern.compile(".*.");
-		Matcher m = p.matcher(matrix);
+	public static int getRowsCount() {
+		ArrayList<String> row = calculateRow(matrixAsString);
+	}
+	
+	
+	public static int getColumnsCount() {
 		
-		while (m.find()) {
-			String trimmed = m.group().replaceAll("\\s", "");
-			rows.add(trimmed);
-		}
+	}
+	
+	public static int getRow(int row) {
 		
-		/*
-		 * line 0 char at 0
-		 * line 1 char at 0
-		 * line 2 char at 0
-		 * 
-		 * line 0 char at 1
-		 * line 1 char at 1
-		 * line 2 char at 1
-		 * 
-		 * line 0 char at 2
-		 * line 1 char at 2
-		 * line 2 char at 2
-		 */
+	}
+	
+	public static int getColumn(int row) {
 		
-		int rowLength = rows.get(0).length();	// get the lenght of a row
+	}
+	
+	public static ArrayList<String> calculateColumn(String matrixAsString) {
+		int rowLength = rows.get(0).length();	// get the length of a row
 		for (int i = 0; i <= rowLength - 1;  i++) { // loop through every column
 			StringBuilder sb = new StringBuilder(); // Create empty string to hold the individual chars (digit on each row)
 			for (int j = 0; j <= rows.size() - 1; j++) { // loop through every row
@@ -41,11 +40,67 @@ public class Matrix {
 			}
 			columns.add(sb.toString()); // Add to array
 		}
-
-		for (String s : columns) {
-			System.out.println(s);
-		}
-
+		return columns;
 	}
+	
+	
+	public static ArrayList<String> calculateRow(String matrixAsString) {
+		Pattern p = Pattern.compile(".*.");
+		Matcher m = p.matcher(matrixAsString);
+		
+		while (m.find()) {
+			String trimmed = m.group().replaceAll("\\s", "");
+			rows.add(trimmed);
+		}	
+		return rows;
+	}
+
+//	public static void main(String[] args) {
+//		ArrayList<String> rows = new ArrayList<String>();
+//		ArrayList<String> columns = new ArrayList<String>();
+//		
+//		String matrix = "9 8 7\n5 3 2\n6 6 7";
+//
+//		Pattern p = Pattern.compile(".*.");
+//		Matcher m = p.matcher(matrix);
+//		
+//		while (m.find()) {
+//			String trimmed = m.group().replaceAll("\\s", "");
+//			rows.add(trimmed);
+//		}
+//		
+//		/*
+//		 * line 0 char at 0
+//		 * line 1 char at 0
+//		 * line 2 char at 0
+//		 * 
+//		 * line 0 char at 1
+//		 * line 1 char at 1
+//		 * line 2 char at 1
+//		 * 
+//		 * line 0 char at 2
+//		 * line 1 char at 2
+//		 * line 2 char at 2
+//		 */
+//		
+//		int rowLength = rows.get(0).length();	// get the length of a row
+//		for (int i = 0; i <= rowLength - 1;  i++) { // loop through every column
+//			StringBuilder sb = new StringBuilder(); // Create empty string to hold the individual chars (digit on each row)
+//			for (int j = 0; j <= rows.size() - 1; j++) { // loop through every row
+//				char c = rows.get(j).charAt(i); // get the digit on the specified index 0,1,2 ...
+//				sb.append(c); // Append to string
+//			}
+//			columns.add(sb.toString()); // Add to array
+//		}
+//		
+//		for (String r : rows) {
+//			System.out.println(r);
+//		}
+//		System.out.println("-------------");
+//		
+//		for (String c : columns) {
+//			System.out.println(c);
+//		}
+//	}
 
 }
