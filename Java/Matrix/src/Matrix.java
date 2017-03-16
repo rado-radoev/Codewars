5c459dc9813ddcee6f17ad11c9d78861d94f0a7f
Radoslav Radoev <rradoev@gmail.com>
2017-03-16 11:42:30 -0700

    Matrix -wip

--- Java/Matrix/src/Matrix.java
+++ Java/Matrix/src/Matrix.java
@@ -1,8 +1,47 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Matrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> rows = new ArrayList<String>();
+		ArrayList<String> columns = new ArrayList<String>();
+		
+		String matrix = "9 8 7\n5 3 2\n6 6 7";
+
+		Pattern p = Pattern.compile(".*.");
+		Matcher m = p.matcher(matrix);
+		
+		while (m.find()) {
+			String trimmed = m.group().replaceAll("\\s", "");
+			rows.add(trimmed);
+		}
+		
+		/*
+		 * line 0 char at 0
+		 * line 1 char at 0
+		 * line 2 char at 0
+		 * 
+		 * line 0 char at 1
+		 * line 1 char at 1
+		 * line 2 char at 1
+		 * 
+		 * line 0 char at 2
+		 * line 1 char at 2
+		 * line 2 char at 2
+		 */
+		
+		for (int i = 0; i < rows.size(); i++) {
+			// loop through row 0
+			// get the lenght of the row - 1
+			for (int j = 0; j < rows.get(i).length() - 1; j++) {
+				int index = rows.get(i).length() - rows.get(i).length() + i;
+				char col = rows.get(i).charAt(index);
+				System.out.println(col);
+				break;
+			}
+		}
 
 	}
 
