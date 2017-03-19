96882aa6d532d60fc56afa31868fae77b5e3105b
superklamer <rradoev@gmail.com>
2017-03-19 15:32:19 -0700

    Encrypt - Decrypt - done

--- Java/Encryption/src/Encryption.java
+++ Java/Encryption/src/Encryption.java
@@ -1,15 +1,6 @@
 ///https://www.codewars.com/kata/simple-encryption-number-1-alternating-split/train/java
 public class Encryption {
 	
-	// Get the string
-	// for every even character 
-	// add to string
-	// if int n <= 0 return the string as it is
-	// if n == 1 return the string modified once
-	// if n == 2 iterate through the modifed string again
-	// if n == 3 iterate throught the modified string 2 again
-	// etc.
-	
 	public static String mainEncrypt(String text) {
 	    char[] textToArray = text.toCharArray();
 	    StringBuilder sbEven = new StringBuilder();
@@ -45,13 +36,13 @@ public class Encryption {
 		    char[] textToArray = text.toCharArray();
 		    StringBuilder output = new StringBuilder();
 		    
-		    int middle = (textToArray.length - 1) / 2;
+		    int middle = (textToArray.length) / 2;
 		    for (int i = 0; i < middle; i++) {
 		    	output.append(textToArray[i]);
 		    }
 		    
 		    int insertCounter = 0;
-		    for (int i = middle; i < textToArray.length - 1; i++) {
+		    for (int i = middle; i < textToArray.length; i++) {
 		    	output.insert(insertCounter, textToArray[i]);
 		    	insertCounter += 2;
 		    }
@@ -69,8 +60,9 @@ public class Encryption {
 		   return output;
 	  }
 	  
-	  public static void main(String[] args) {
-		  String text = "hsi  etTi sats!!";
-		  System.out.println(mainDecrypt(text));
+	  public static void main (String[] args) {
+		  String text = "hskt svr neetn!Ti aai eyitrsig";
+		  System.out.println(decrypt(text, 1));
+		  
 	  }
 }
--- Java/Encryption/src/EncryptionTest.java
+++ Java/Encryption/src/EncryptionTest.java
@@ -5,7 +5,7 @@ import org.junit.Ignore;
 
 public class EncryptionTest {
 
-	@Ignore
+
 	@Test
 	  public void testEncrypt() {
 		    // assertEquals("expected", "actual");
@@ -21,11 +21,11 @@ public class EncryptionTest {
 	  public void testDecrypt() {
 	    // assertEquals("expected", "actual");
 	    assertEquals("This is a test!", Encryption.decrypt("This is a test!", 0));
-//	    assertEquals("This is a test!", Encryption.decrypt("hsi  etTi sats!", 1));
+	    assertEquals("This is a test!", Encryption.decrypt("hsi  etTi sats!", 1));
 	    assertEquals("This is a test!", Encryption.decrypt("s eT ashi tist!", 2));
-//	    assertEquals("This is a test!", Encryption.decrypt(" Tah itse sits!", 3));
-//	    assertEquals("This is a test!", Encryption.decrypt("This is a test!", 4));
-//	    assertEquals("This is a test!", Encryption.decrypt("This is a test!", -1));
-//	    assertEquals("This kata is very interesting!", Encryption.decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+	    assertEquals("This is a test!", Encryption.decrypt(" Tah itse sits!", 3));
+	    assertEquals("This is a test!", Encryption.decrypt("This is a test!", 4));
+	    assertEquals("This is a test!", Encryption.decrypt("This is a test!", -1));
+        assertEquals("This kata is very interesting!", Encryption.decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
 	  }
 }
