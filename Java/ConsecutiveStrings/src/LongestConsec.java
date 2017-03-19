163d8bdbb2a14cb5140db5f438830ede1dfc9c9a
superklamer <rradoev@gmail.com>
2017-03-19 15:55:37 -0700

    Longest consecutive string - wip

---
+++ Java/ConsecutiveStrings/src/LongestConsec.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/consecutive-strings/train/java
+public class LongestConsec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
