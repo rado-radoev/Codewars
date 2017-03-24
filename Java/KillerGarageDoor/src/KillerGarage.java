d61e77f405f5e9dfbf2961856dce1db991515464
superklamer <rradoev@gmail.com>
2017-03-23 19:22:57 -0700

    Killer Garage Door - wip

---
+++ Java/KillerGarageDoor/src/KillerGarage.java
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/killer-garage-door/train/java
+public class KillerGarage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
