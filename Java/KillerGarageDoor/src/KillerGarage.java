a154e8f1a522c02640582440a647da7def0e2d73
superklamer <rradoev@gmail.com>
2017-03-23 20:02:16 -0700

    Killer Garage Door - wip

--- Java/KillerGarageDoor/src/KillerGarage.java
+++ Java/KillerGarageDoor/src/KillerGarage.java
@@ -6,4 +6,26 @@ public class KillerGarage {
 
 	}
 
+	// Convert string to char array
+	// 0 fully closed
+	// 5 fully opened
+	// . nothing
+	// P Button is pressed - if no previous P -> Open
+	// if Other P > close
+	// O -> obstacle detected -  reverse counting;
+	// if less than 1 -> zeros
+	// if higher than 5 -> zeroes
+	
+	/*
+	 * char[] arr = string.toCharArray();
+	 * iterate through the array
+	 * set a counter that is equal to the array length
+	 * char previous = char[i - 1];
+	 * char next = char[i + 1]
+	 * char current = char[i]
+	 * StringBuilder output = new StringBuilder()
+	 * 
+	 * 
+	 * 
+	 */
 }
