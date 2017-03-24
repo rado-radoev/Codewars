2a52707e3953bf7cd7ef7879abcb7b5939824c1c
superklamer <rradoev@gmail.com>
2017-03-23 19:15:09 -0700

    Concatenated String - done

--- Java/ConsecutiveStrings/src/LongestConsec.java
+++ Java/ConsecutiveStrings/src/LongestConsec.java
@@ -3,48 +3,19 @@ import java.util.HashMap;
 public class LongestConsec {
 
 	public static void main(String[] args) {
-		String[] s = {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"};
-		String[] s2 = {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"};
-		String[] s3 = {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"};
-		longestConsec(s2, 1);
+		String[] s = {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"};
+		System.out.println(longestConsec(s, 3));
 
 	}
 	
-	/*
-	 * {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2), "abigailtheta");
-	 * int longestStrLenght = Integer.MinValue;
-	 * String longestString = null;
-	 * 
-	 * for int i = arr.lenght
-	 * if (arr[i].lenght > longestStrLenght)
-	 * 		longestStrLenght == arr[i].lenght
-	 * 		longestString = arr[i]
-	 * 
-	 * check string 0 + 1
-	 * conatenate and get lenght
-	 * store in hash map lenght > string
-	 * check string 1 + 2
-	 * concatenate and get lenght
-	 * store in hash map lenght > string
-	 * 
-	 * loop through has map
-	 * int maxValue = Integer.Min;
-	 * String longestConcatStr = null;
-	 * if hashmap key > maxValue
-	 * 		maxvalue = hashmapkey
-	 * 		longestConcatStr = hashmapkey.getvalue
-	 * 
-	 * 
-   	 * {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1), "oocccffuucccjjjkkkjyyyeehh");
-   	 * testing(LongestConsec.longestConsec(new String[] {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
-        testing(LongestConsec.longestConsec(new String[] {"wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"}, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
-        testing(LongestConsec.longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2), "");
-        testing(LongestConsec.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 3), "ixoyx3452zzzzzzzzzzzz");
-        testing(LongestConsec.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15), "");
-        testing(LongestConsec.longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 0), "");
-	 * */ 
-
-	public static void longestConsec (String[] s, int k) {
+	public static String longestConsec (String[] s, int k) {
+		
+		final int n = s.length;
+		if (n == 0 || k > n ||k <=0) {
+			return "";
+		}
+		
+		
 		HashMap<Integer, String> myHashMap = new HashMap<Integer, String>();
 		StringBuilder tempStr = new StringBuilder();
 		int longestStrLenght = Integer.MIN_VALUE;
@@ -56,18 +27,24 @@ public class LongestConsec {
 				longestString = s[i];
 			}
 			
-			if (i < s.length - 1) {
-				tempStr.append(s[i] + s[i + 1]);
-			}
-			else {
-				tempStr.append(s[i]);
+			
+			int counter = 0;
+			while (counter < k ) {
+				if (i + counter < n) {
+					tempStr.append(s[i + counter]);
+				}
+				
+				counter++;
 			}
 			
-			System.out.printf("String added to hashMap: %s%n", tempStr.toString());
 			if (!myHashMap.containsKey(tempStr.length())) {
 				myHashMap.put(tempStr.length(), tempStr.toString());
 			}
 			tempStr.delete(0, tempStr.length());
+		}
+		
+		if (k == 1) {
+			return longestString;
 		}
 		
 		
@@ -79,6 +56,6 @@ public class LongestConsec {
 				longestConcatenatedString = myHashMap.get(key);
 			}
 		}
-			System.out.println(longestConcatenatedString);	
+			return longestConcatenatedString;	
 	}
 }
