35f81f1568f611997c73aa5b8f7c3c5c31b9603e
Radoslav Radoev <rradoev@gmail.com>
2017-03-27 10:17:29 -0700

    Multiples - completed - added test cases

--- Java/Multiples/src/Multiples.java
+++ Java/Multiples/src/Multiples.java
@@ -1,9 +1,35 @@
 //https://www.codewars.com/kata/multiples-of-3-and-5/train/java
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Multiples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = input.nextInt();
+		
+		System.out.println(solution(num));
 
+	}
+	
+	public static int solution(int number) {
+		HashSet<Integer> numList = new HashSet<Integer>();
+		
+		for (int i = 1; i < number; i++) {
+			if (i % 3 == 0 || i % 5 == 0 ) {
+				numList.add(i);
+			}
+		}
+		
+		int sum = 0;
+		Iterator<Integer> hashIterator = numList.iterator();
+		while (hashIterator.hasNext()) {
+			sum += hashIterator.next();
+		}
+		
+		return sum;
 	}
 
 }
--- Java/Multiples/src/MultiplesTest.java
+++ Java/Multiples/src/MultiplesTest.java
@@ -1,12 +1,33 @@
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Ignore;
 
 import org.junit.Test;
 
 public class MultiplesTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Multiples multiples;
+	
+	@Before
+	public void setUp() {
+		multiples = new Multiples();
 	}
+	
+	@Test
+	public void testTen() {
+		assertEquals(23, multiples.solution(10));
+	}
+	
 
+	@Test
+	public void testTwenty() {
+		assertEquals(78, multiples.solution(20));
+	}
+	
+	@Ignore
+	@Test
+	public void TestOneThousand() {
+		assertEquals(233168, multiples.solution(1000));
+	}
+	
 }
