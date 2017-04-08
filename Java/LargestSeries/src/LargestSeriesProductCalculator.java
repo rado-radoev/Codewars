f5bdf6f5d6ac4d1a3fb5d4a836955c918f415867
superklamer <rradoev@gmail.com>
2017-04-07 18:22:52 -0700

    LargestSeries done

--- Java/LargestSeries/src/LargestSeriesProductCalculator.java
+++ Java/LargestSeries/src/LargestSeriesProductCalculator.java
@@ -1,40 +1,63 @@
 
+/**
+ * @author superlamer
+ *
+ */
 public class LargestSeriesProductCalculator {
 
 	private String str;
 	private long[] numArray;
 	
+	/**
+	 * LargestSeriesProductCalculator constructor
+	 * @param str	String to evaluate
+	 * @throws IllegalArgumentException 	String should not be empty and should only contain numbers
+	 */
 	public LargestSeriesProductCalculator(String str) throws IllegalArgumentException {
 		setStr(str);
 		this.numArray = convertStrToIntArray(str);
 	}
 	
 	
-	
-	public void setStr(String str) {
-		if (str.length() == 0) {
-			throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
-		}
-		else if (str.isEmpty()) {
+	/**
+	 * String setter
+	 * @param str 	String to evaluate
+	 * @throws IllegalArgumentException	String should not be null
+	 */
+	private void setStr(String str) throws IllegalArgumentException {
+		if (str == null) {
 			throw new IllegalArgumentException("String to search must be non-null.");
 		}
-		
 		this.str = str;
 	}
 	
 	
+	/**
+	 * convertStrToIntArray helper method. Converts string to Integer array
+	 * @param str	String to convert
+	 * @return		Integer array
+	 */
 	private long[] convertStrToIntArray(String str) {
 		long[] numArray = new long[str.length()];
 		for (int i = 0; i < str.length(); i++) {
+			if (!Character.isDigit(str.charAt(i))) {
+				throw new IllegalArgumentException("String to search may only contains digits.");
+			}
 			numArray[i] = str.charAt(i) - 48;
 		}
 		return numArray;
 	}
 	
+	/**
+	 * calculateLargestProductForSeriesLength	Calculates the largest product for a contiguous substring of digits of length n
+	 * @param n		Number of digits to evaluate
+	 * @return	the largest product for a contiguous substring of digits of length n
+	 * @throws IllegalArgumentException		If n is bigger than the string length. If n is 0 or lower.
+	 */
 	public long calculateLargestProductForSeriesLength(long n) throws IllegalArgumentException {
 		
 		if (n == 0) {
-			
+			return 1;
 		}
 		
 		if (n > str.length()) {
@@ -45,30 +68,22 @@ public class LargestSeriesProductCalculator {
 		}
 		
 		
-		long maxSum = Integer.MIN_VALUE;
-		for (int i = 0; i < numArray.length;i++) {
-			long sum = 1;
-			if (i > numArray.length - n) {
+		long maxSum = Integer.MIN_VALUE;				// Get an absolute min value
+		for (int i = 0; i < numArray.length;i++) {		// Loop throught the array
+			long sum = 1;								// Dummy variable to be recalculated for every n group of numbers
+			if (i > numArray.length - n) {				// Break out of the loop if the current index + n group of numbers will exceed the array lenght
 				break;
 			}
 			
-			for (int j = 0; j < n; j++) {
-				sum *= numArray[i + j];
+			for (int j = 0; j < n; j++) {				// Loop n number of digits
+				sum *= numArray[i + j];					// Add the digit at the current index to sum
 			}
 			
-			if (sum > maxSum) {
-				maxSum = sum;
+			if (sum > maxSum) {							// If the calculated sum is bigger than maxSum
+				maxSum = sum;							// reassign maxSum to be equal to the sum
 			}
 		}
-		
-		return maxSum;
+		return maxSum;									// return maxSum
 	}
-//	
-//	public static void main(String[] args) {
-//		String s = "0123456789";
-//		LargestSeriesProductCalculator_1 largest = new LargestSeriesProductCalculator_1(s);
-//		System.out.println(largest.calculateLargestProductForSeriesLength(3));
-//		
-//	}
-	
+
 }
