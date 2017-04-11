581f2b76bbbc1b7b3afed87061338dbd33fef5f1
Radoslav Radoev <rradoev@gmail.com>
2017-04-11 15:05:49 -0700

    bitinvert -wip

---
+++ Java/BitInvert/src/BitInvert.java
@@ -0,0 +1,13 @@
+
+public class BitInvert {
+
+	public static void main(String[] args) {
+		System.out.println(invertBits(5));
+
+	}
+	
+	public static int invertBits(int n) {
+		return ~n;
+	}
+
+}
