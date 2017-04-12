f5025c61654d2ca60427f989204ac48d178978b3
Radoslav Radoev <rradoev@gmail.com>
2017-04-12 16:28:15 -0700

    NextGreaterElement -wip

--- Java/NextGreaterElement/src/NextGreaterElement.java
+++ Java/NextGreaterElement/src/NextGreaterElement.java
@@ -6,6 +6,11 @@ public class NextGreaterElement {
 	}
 
 	public static int[] nextGreaterElement(int[] findNums, int[] nums) {
+        int[] nums = new int[findNums.length];
+		int j = 0;
         
+        for (int i = 0; i < nums.length; i++) {
+			
+		}
     }
 }
