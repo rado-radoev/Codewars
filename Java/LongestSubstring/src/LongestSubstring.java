c447cb8f52b91f51883034a0fa6859e3fa0af43b
Radoslav Radoev <rradoev@gmail.com>
2017-05-03 16:31:33 -0700

    Longest string - not happy about it

--- Java/LongestSubstring/src/LongestSubstring.java
+++ Java/LongestSubstring/src/LongestSubstring.java
@@ -1,12 +1,31 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/#/description
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class LongestSubstring {
 
 	public static void main(String[] args) {
 		// Given a string, find the length of the longest substring without repeating character
-
+		System.out.println(lengthOfLongestSubstring("abcabcbb"));
 
 	}
+	
+	public static int lengthOfLongestSubstring(String s) {
+	    int i = 0, j = 0, max = 0;
+	    Set<Character> set = new HashSet<>();
+	    
+	    while (j < s.length()) {
+	        if (!set.contains(s.charAt(j))) {
+	            set.add(s.charAt(j));
+	            max = Math.max(max, set.size());
+	            j++;
+	        } else {
+	            set.remove(s.charAt(i));
+	            i++;
+	        }
+	    }
+	    
+	    return max;
+	}
 }
-
