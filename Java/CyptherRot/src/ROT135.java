5bdb732b537716596269e35698e8a26bd80267d9
Radoslav Radoev <rradoev@gmail.com>
2017-05-03 14:24:26 -0700

    Longest string - wp

---
+++ Java/CyptherRot/src/ROT135.java
@@ -0,0 +1,9 @@
+
+public class ROT135 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/LongestSubstring/src/LongestSubstring.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/#/description
+import java.util.ArrayList;
+
+public class LongestSubstring {
+
+	public static void main(String[] args) {
+		// Given a string, find the length of the longest substring without repeating character
+
+
+	}
+}
+
