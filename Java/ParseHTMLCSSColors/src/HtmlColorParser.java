fd2a1e7630307ea4c568f48b3a6afd9c32132790
Radoslav Radoev <rradoev@gmail.com>
2017-05-05 15:49:43 -0700

    HTML/CSS Colors parser - wip

---
+++ Java/ParseHTMLCSSColors/src/HtmlColorParser.java
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/parse-html-slash-css-colors/train/java
+public class HtmlColorParser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
