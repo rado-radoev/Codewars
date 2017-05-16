1b3d8f9295ad548af4bf41b2457eb50a80839013
Radoslav Radoev <rradoev@gmail.com>
2017-05-16 16:48:05 -0700

    Exercism nth prime

--- Java/Exercism_nThPrime/src/PrimeCalculator.java
+++ Java/Exercism_nThPrime/src/PrimeCalculator.java
@@ -1,5 +1,9 @@
-
+//http://exercism.io/exercises/java/nth-prime/readme
 public class PrimeCalculator {
+	
+	// Create a boolean array, set to false by default.
+	// that array will contain all prime numbers to the square root of INTEGER MAX VALUE
+	private static boolean[] arr = new boolean[(int)Math.sqrt(Integer.MAX_VALUE) + 1];
 
 	public static void main(String[] args) {
 		// Use the sieve algorithm
@@ -8,7 +12,25 @@ public class PrimeCalculator {
 		// increment counter
 		// when the counter == the nTh number 
 		// return the prime
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println("Square root of Integer.MAX_VALUE = " + (int)Math.sqrt(Integer.MAX_VALUE));
 
+	}
+	
+	private static void findAllPrimes() {
+		int nSqrt = (int)Math.sqrt(Integer.MAX_VALUE);
+		
+		for (int i = 2; i <= nSqrt; i++) {
+			if (!arr[i]) {
+				
+			}
+		}
+	}
+	
+	public static int findNprime (int nthPrime) {
+		// this method to look up the nth (index in the array containing all primes)
+		return 0;
 	}
 
 }
