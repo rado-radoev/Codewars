df227418c2b61e45e377a8b02ab8d3c482d4e652
superklamer <rradoev@gmail.com>
2017-05-16 19:42:36 -0700

    Exercism - Prime Calculator - done. Exercism DiamontPointer - pseudo code almost done.

---
+++ Java/Exercism_DiamondPrinter/src/Exercism_DiamondPrinter.java
@@ -0,0 +1,37 @@
+//http://exercism.io/exercises/java/diamond/readme
+// asci char map: http://www.asciitable.com/
+
+public class Exercism_DiamondPrinter {
+
+	public static void main(String[] args) {
+		/*
+		 * letter A = 65
+		 * letter Z = 90
+		 * max loop is max number of letters = 26
+		 * 
+		 * 
+		 * 
+		 * Get the number of letter supplied as argument
+		 * subtract the number from 90, the result subtract from 26
+		 * that will be the number of loops you have to make
+		 * EXAMPLE: 
+		 * 
+		 * argument is C (67)
+		 * subtract from Z (90)
+		 * 90 - 67 = 23
+		 * max letters (26) - 23 = 3 iterations
+		 * 3 would be the highest point of the diamond 
+		 * at index 1 print A = 65
+		 * at index 2 print B = 66
+		 * at index 3 print C = 67 (highest point)
+		 * START LOOPING BACKWARDS
+		 * at index 2 print B = 66
+		 * at index 1 print A = 65 
+		 *
+		 * starting row 1 add the number of iterations as spaces before and after the letter
+		 * 
+		 * */
+
+	}
+
+}
--- Java/Exercism_nThPrime/src/PrimeCalculator.java
+++ Java/Exercism_nThPrime/src/PrimeCalculator.java
@@ -1,36 +1,63 @@
 //http://exercism.io/exercises/java/nth-prime/readme
 public class PrimeCalculator {
 	
-	// Create a boolean array, set to false by default.
-	// that array will contain all prime numbers to the square root of INTEGER MAX VALUE
-	private static boolean[] arr = new boolean[(int)Math.sqrt(Integer.MAX_VALUE) + 1];
-
-	public static void main(String[] args) {
-		// Use the sieve algorithm
-		// Add counter
-		// every time a prime has been found
-		// increment counter
-		// when the counter == the nTh number 
-		// return the prime
-		
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println("Square root of Integer.MAX_VALUE = " + (int)Math.sqrt(Integer.MAX_VALUE));
-
+	// Create an array of primes that will hold up to 20 000 primes
+	private static int[] primes = new int[20000];
+	
+	// default constructor that runs the findAllPrimes method when class is called
+	public PrimeCalculator() {
+//		long before = System.currentTimeMillis();
+		findAllPrimes();
+//		long after = System.currentTimeMillis();
+//		System.out.println("findAllPrimes Elapsed time: " + Long.toString(after - before) + " milliseconds");
 	}
 	
-	private static void findAllPrimes() {
-		int nSqrt = (int)Math.sqrt(Integer.MAX_VALUE);
+	private final static void findAllPrimes() {
 		
-		for (int i = 2; i <= nSqrt; i++) {
-			if (!arr[i]) {
-				
+		// array to hold up to 200 000 booleans
+		// we don't need that array outside of the method, so we'll let the garbage collector
+		// remove it once the method is closed
+		boolean[] arr = new boolean[200000];
+	
+		// set all values in the array to true
+		// this takes time, but will not take memory if we had put the array in the class as an instance
+//		long before = System.currentTimeMillis();
+		zeroOutBooleanArray(arr);
+//		long after = System.currentTimeMillis();
+//		System.out.println("zeroOutBooleanArray Elapsed time: " + Long.toString(after - before) + " milliseconds");		
+		
+		// variable to set the array index to add the prime number to
+		// index 0 should not be used. start from index 1
+		int arrayIndex=1;
+		
+		// loop until the array length is reached
+		for (int i = 2; i < arr.length; i++) {
+			if (arr[i]) {	// If the index is prime go through all multiples and make their values false (they are not primes)		
+				primes[arrayIndex++] = i;	// if true add the index to the primes array (that is a prime number)
+				for (int j = 2; i * j < arr.length; j++) {
+					arr[i*j] = false;	// make all the multiples of i - false
+				}
 			}
 		}
 	}
 	
-	public static int findNprime (int nthPrime) {
-		// this method to look up the nth (index in the array containing all primes)
-		return 0;
+	// used to set a boolean array to all true	
+	private static boolean[] zeroOutBooleanArray (boolean[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = true;
+		}
+		return array;
 	}
 
+	
+	// main method
+	// looks up the primes array and returns the number at the specified index
+	// the nthPrime should be the the element at nth index in the primes array
+	public static int nth (int nthPrime) throws IllegalArgumentException {
+		if (nthPrime == 0) {
+			throw new IllegalArgumentException();
+		}
+		return primes[nthPrime];
+	}
 }
