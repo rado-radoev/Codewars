3fc95067d92b58d5f2a1b6693840e737c3a8e9c6
Radoslav Radoev <rradoev@gmail.com>
2017-05-23 09:06:14 -0700

    exercism flatten array - maven proj setup

---
+++ Java/Exercism_FlattenArray/src/main/java/com/exercism/Flattener.java
@@ -0,0 +1,13 @@
+//http://exercism.io/exercises/java/flatten-array/readme
+package com.exercism;
+
+public class Flattener {
+	
+	public static void flattener() {}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
