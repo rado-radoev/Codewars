a92392c7a0914cb86e443ff8e17237967b44f8b9
Radoslav Radoev <rradoev@gmail.com>
2017-05-24 16:11:12 -0700

    exercism piglatin

--- Java/Exercism_PigLatin/src/main/java/com/exercism/PigLatinTranslator.java
+++ Java/Exercism_PigLatin/src/main/java/com/exercism/PigLatinTranslator.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class PigLatinTranslator {
 	
 	HashSet<String> vowels = new HashSet<String>();
+	HashSet<String> specialConsonants = new HashSet<String>();
 	
 	private void populateVowels() {
 		vowels.add("a");
@@ -12,9 +13,19 @@ public class PigLatinTranslator {
 		vowels.add("i");
 		vowels.add("o");
 		vowels.add("u");
+		vowels.add("yt");
+		vowels.add("xt");
+	}
+	
+	private void populateSpecialConsonants() {
+		specialConsonants.add("ch");
+		specialConsonants.add("qu");
+		specialConsonants.add("th");
+		specialConsonants.add("thr");
+		specialConsonants.add("sch");
 	}
 	
 	public String translate(String text) {
-		return null;
+		
 	}
 }
