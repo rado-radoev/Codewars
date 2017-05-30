4bb7e9eab5160cf5b38a907ffeebcd2b8ee7660c
Radoslav Radoev <rradoev@gmail.com>
2017-05-29 20:04:28 -0700

    exercism phonenumber -wip

--- Java/Exercism-PhoneNumber/src/main/java/com/exercism/PhoneNumber.java
+++ Java/Exercism-PhoneNumber/src/main/java/com/exercism/PhoneNumber.java
@@ -1,5 +1,14 @@
 package com.exercism;
 
 public class PhoneNumber {
+	
+	private String phoneNumber;
+	
+	public PhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
+	public String getNumber() {
+		return null;
+	}
 }
