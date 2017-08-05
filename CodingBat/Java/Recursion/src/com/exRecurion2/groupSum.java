edf273fd23ae39d8a8fe9194a64f4af53ce45e94
superklamer <rradoev@gmail.com>
2017-08-04 22:57:33 -0700

    recursion with backtracking

--- CodingBat/Java/Recursion/src/com/exRecurion2/groupSum.java
+++ CodingBat/Java/Recursion/src/com/exRecurion2/groupSum.java
@@ -20,9 +20,43 @@ public class groupSum {
 		System.err.println(groupSum(0, arr, 10));
 	}
 
-	public boolean groupSum(int start, int[] sums, int target) {
-		if (start >= sums.length)
-			return false;
+	public static boolean groupSum(int start, int[] nums, int target) {
+
+		  // Base case: if there are no numbers left, then there is a
+		  // solution only if target is 0.
+		  if (start >= nums.length) return (target == 0);
+		  
+		  // Key idea: nums[start] is chosen or it is not.
+		  // Deal with nums[start], letting recursion
+		  // deal with all the rest of the array.
+		  
+		  // Recursive call trying the case that nums[start] is chosen --
+		  // subtract it from target in the call.
+		  if (groupSum(start + 1, nums, target - nums[start])) return true;
+		  
+		  // Recursive call trying the case that nums[start] is not chosen.
+		  if (groupSum(start + 1, nums, target)) return true;
+		  
+		  // If neither of the above worked, it's not possible.
+		  return false;		
+		
+		
+		
+		
+		
+		
+//		if (target == 0)
+//			return true;
+//		
+//		if (start == nums.length)
+//			return false;
+//		
+//
+//		
+//		if (groupSum(start + 1, nums, target - nums[start]))
+//			return true;
+//		
+//		return groupSum(start + 1, nums, target);
 		
 		
 	}
