adeebc7d1e31b054cc5abcd91ddb3cb13cf6b842
superklamer <rradoev@gmail.com>
2017-08-05 21:59:20 -0700

    functions. but I can't think right noow .witll take it on tomorrow

---
+++ CodingBat/Java/Functional/src/com/Functional1/Doubling.java
@@ -0,0 +1,24 @@
+package com.Functional1;
+
+/*Given a list of integers, return a list where each integer is multiplied by 2.*/
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Doubling {
+
+	public static void main(String[] args) {
+		List<Integer> nums = new ArrayList<Integer>();
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+	
+		System.out.println();
+	}
+	
+	
+	public List<Integer> doubling(List<Integer> nums) {
+		
+	}
+}
