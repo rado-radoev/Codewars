c7c97d456c65045a0a6b0f5a95ddd0a378316a11
Radoslav Radoev <rradoev@gmail.com>
2018-01-15 22:52:09 -0800

    HackerRank 30 day challenge, day 9

--- HackerRank 30day challenge/Day8/PhoneBook/src/Solution.java
+++ HackerRank 30day challenge/Day8/PhoneBook/src/Solution.java
@@ -1,48 +1,29 @@
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Solution{
+class Solution{
 	
-	private static Dictionary<String, Integer> phoneBook = new Hashtable<>();
+	private static Map<String, Integer> phoneBook = new HashMap<String, Integer>();
 	
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             phoneBook.put(name, phone);
         }
-        
-        Enumeration<String> names = phoneBook.keys();
-        
         while(in.hasNext()){
             String s = in.next();
             
-            boolean nameExists = false;
-            while (names.hasMoreElements()) {
-				String name = (String) names.nextElement();
-				if (s.equals(name)) {
-					nameExists = true;
-					break;
-				}
-			}
-            
-            if (!nameExists) {
-            		System.out.println("Not found");
-            		continue;
+            if (phoneBook.containsKey(s)) {
+            		System.out.printf("%s=%d%n", s, phoneBook.get(s));
             }
-        
-            try {
-    				Integer phoneNumber = phoneBook.get(s);
-    				System.out.printf("%s=%d%n", s, phoneNumber);
-    			} catch (NullPointerException pre) {
-    				pre.printStackTrace();
-    			}
-                    
+            else {
+            		System.out.println("Not found");
+            }
+            
         }
         in.close();
     }
