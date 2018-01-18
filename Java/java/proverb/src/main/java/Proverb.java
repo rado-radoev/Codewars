744b858e68a47055b555b8c4c8049ee36032287e
Radoslav Radoev <rradoev@gmail.com>
2018-01-17 23:14:26 -0800

    Exercism proverb

--- Java/java/proverb/src/main/java/Proverb.java
+++ Java/java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,34 @@
 class Proverb {
 
+	String[] words;
+	
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuffer sb = new StringBuffer();
+    		
+        if (words.length == 0)
+        		return "";
+        else if (words.length == 1)
+        		return sb.append(String.format("And all for the want of a %s.", words[0])).toString();
+        else {
+	        	for (int i = 0; i < words.length - 1; i++) {
+	        		sb.append(String.format("For want of a %s the %s was lost.%n", words[i], words[i+1]));
+	        }
+	        
+	        sb.append(String.format("And all for the want of a %s.", words[0]));
+        }
+        	
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) {
+    	String[] words  = new String[]{"nail", "shoe", "horse", "rider", "message", "battle", "kingdom"};   	
+    	Proverb proverb = new Proverb(words);
+    		
+    		proverb.recite();
     }
 
 }
