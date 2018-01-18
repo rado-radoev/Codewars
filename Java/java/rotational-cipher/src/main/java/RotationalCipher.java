f1ed12396b6aaedebeccc33ad6219fa1777e0f6e
Radoslav Radoev <rradoev@gmail.com>
2018-01-18 15:59:02 -0800

    Exercism rotational cipher

--- Java/java/rotational-cipher/src/main/java/RotationalCipher.java
+++ Java/java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,43 @@
 class RotationalCipher {
+	
+	int shiftKey;
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    	this.shiftKey = shiftKey;
     }
 
-    String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    boolean isInrange(int x, int min, int max) {
+    	return x >= min && x <= max;
+    }
+   
+    String rotate(String data) { 	
+    	   	
+    	StringBuffer sb = new StringBuffer(data.length());
+    	
+    	for (int i = 0; i < data.length(); i++) {
+
+    		
+    		int c = (int)data.charAt(i);
+    		int res = c;
+    		
+    		if (isInrange(c, 65, 90)) {
+    			res = c + shiftKey;
+    			if (res > 90) {
+    				res = (res - 90) + 64;
+    			}
+    		}
+    		else if (isInrange(c, 97, 122)) {
+    			res = c + shiftKey;
+    			if (res > 122) {
+    				res = (res - 122) + 96;
+    			}
+    		}
+    		
+    		sb.append((char)(res));
+    		  		
+    	}
+    	
+    	return sb.toString();
     }
 
 }
