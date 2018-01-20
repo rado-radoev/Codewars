4f0977c148d1f56fe4a373bf1e94bbf44eb0a416
Radoslav Radoev <rradoev@gmail.com>
2018-01-20 00:30:28 -0800

    exercism collatz conjecture -done WITH RECURSION

--- Java/java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ Java/java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,7 +1,22 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    		if (start <= 0)
+    			throw new IllegalArgumentException("Only natural numbers are allowed");
+        return (computeStepCount(start, 0));
     }
 
+    int computeStepCount(int start, int count) {
+    		if (start == 1)
+    			return count;
+    		
+    		count++;
+    		
+    		if (start % 2 == 0)
+    			start /= 2;
+    		else
+    			start = (start * 3) + 1;
+    		
+    		return computeStepCount(start, count);
+    }
 }
