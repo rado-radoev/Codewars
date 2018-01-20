606bc6741c2e874a1d0325eb9039836c10839957
Radoslav Radoev <rradoev@gmail.com>
2018-01-19 23:21:11 -0800

    exercism kinder garden - wip

---
+++ Java/java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class KindergartenGarden {
+	
+	private String[] students;
+	private String garden;
+
+    KindergartenGarden(String garden, String[] students) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    KindergartenGarden(String garden) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    List<Plant> getPlantsOfStudent(String student) {
+       
+    }
+
+}
