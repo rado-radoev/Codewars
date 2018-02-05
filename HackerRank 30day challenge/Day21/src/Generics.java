48228b5fd4000d087707dea3b5098b0fa01ab055
Radoslav Radoev <rradoev@gmail.com>
2018-02-05 14:18:01 -0800

    Hacker rank day21 Generics

---
+++ HackerRank 30day challenge/Day21/src/Generics.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+ class Printer<T> {  
+    public <E> void printArray(E[] A) {
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(A[i]);
+        }
+    }
+ }
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        scanner.close();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
